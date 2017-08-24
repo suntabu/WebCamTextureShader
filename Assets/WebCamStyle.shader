ccd4a097a293f24d72c6fdb079b1a60733304665
gouzhun <gouzhun@nexa-corp.com>
2017-08-24 23:04:46 +0800

    init

---
+++ Assets/WebCamStyle.shader
@@ -0,0 +1,133 @@
+﻿Shader "_Game/WebCamStyle"
+{
+	Properties
+	{
+		_MainTex ("Texture", 2D) = "white" {}
+		_CoverTex ("Texture", 2D) = "black" {}
+		_FlipStyle("Flip Style",Range(-15,15)) = 0
+	}
+	SubShader
+	{
+		Tags
+		{ 
+			"Queue"="Transparent" 
+			"IgnoreProjector"="True" 
+			"RenderType"="Transparent" 
+			"PreviewType"="Plane"
+			"CanUseSpriteAtlas"="True"
+		}
+		LOD 100
+		Lighting Off
+		ZWrite Off
+		Blend SrcAlpha OneMinusSrcAlpha
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			// make fog work
+			#pragma multi_compile_fog
+			
+			#include "UnityCG.cginc"
+
+			struct appdata
+			{
+				float4 vertex : POSITION;
+				float4 color    : COLOR;
+				float2 uv : TEXCOORD0;
+			};
+
+			struct v2f
+			{
+				float4 uv : TEXCOORD0;
+				UNITY_FOG_COORDS(1)
+				float4 vertex : SV_POSITION;
+				float4 color    : COLOR;
+			};
+
+			sampler2D _MainTex;
+			sampler2D _CoverTex;
+			float4 _MainTex_ST;
+			float4 _CoverTex_ST;
+
+			float _FlipStyle;
+			v2f vert (appdata v)
+			{
+				v2f o;
+				o.vertex = UnityObjectToClipPos(v.vertex);
+				o.uv.xy = TRANSFORM_TEX(v.uv, _MainTex);
+				o.uv.zw = TRANSFORM_TEX(v.uv, _CoverTex);
+				o.color = v.color;
+				UNITY_TRANSFER_FOG(o,o.vertex);
+				return o;
+			}
+			
+			fixed4 frag (v2f i) : SV_Target
+			{
+				// sample the texture
+				fixed2 mainCoord;
+				if(_FlipStyle == 1){
+					mainCoord = fixed2(1-i.uv.x, i.uv.y);
+				}else if(_FlipStyle == 2){
+					mainCoord = fixed2(i.uv.y,i.uv.x);
+				}
+
+				else if(_FlipStyle == 3){
+					mainCoord = fixed2( i.uv.x, 1-i.uv.y);   
+				}
+				else if(_FlipStyle == 4){
+					mainCoord = fixed2(1-i.uv.y,1-i.uv.x);  
+				}
+				else if(_FlipStyle == 5){
+					mainCoord = fixed2( i.uv.x,1-i.uv.y);  //
+				}
+				else if(_FlipStyle == 6){
+					mainCoord = fixed2( i.uv.y,i.uv.x);  //
+				}
+				else if(_FlipStyle == 7){
+					mainCoord = fixed2(1-i.uv.x,i.uv.y);  //
+				}
+				else if(_FlipStyle == 8){
+					mainCoord = fixed2(i.uv.y, i.uv.x);  //
+				}
+
+				else if(_FlipStyle == -1){
+					mainCoord = fixed2(  i.uv.x,i.uv.y); //
+				}else if(_FlipStyle == -2){
+					mainCoord = fixed2(i.uv.y,1-i.uv.x);  //
+				}
+				else if(_FlipStyle == -3){
+					mainCoord = fixed2( 1-i.uv.x,1-i.uv.y); //
+				}
+				else if(_FlipStyle == -4){
+					mainCoord = fixed2(1-i.uv.y, i.uv.x); //
+				}else if(_FlipStyle == -5){
+					mainCoord = fixed2(i.uv.x,1-i.uv.y);   //
+				}
+				else if(_FlipStyle == -6){
+					mainCoord = fixed2(1-i.uv.y, 1-i.uv.x);  //
+				}
+				else if(_FlipStyle == -7){
+					mainCoord = fixed2(1-i.uv.x,i.uv.y);   //
+				}
+				else if(_FlipStyle == -8){
+					mainCoord = fixed2(i.uv.y,i.uv.x);   //
+				}
+				else
+				{
+					mainCoord = i.uv.xy;
+				}
+
+
+				fixed4 col = tex2D(_MainTex, mainCoord);
+				fixed4 cover= tex2D(_CoverTex,i.uv.zw);
+
+				col = col * (1- cover.a) + cover.a * cover; 
+				col *=i.color;
+//				col  = fixed4(1,1,0,0.3);
+				return col;
+			}
+			ENDCG
+		}
+	}
+}
---
+++ Assets/test.cs
@@ -0,0 +1,162 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class test : MonoBehaviour
+{
+	public RawImage Source;
+	public RawImage Target;
+
+
+	public bool requestIsFrontFacing;
+	public bool isVerticalMirror;
+	private int angle;
+
+	void Start ()
+	{
+		
+	}
+
+
+	int ConvertCode ()
+	{
+		angle = 360 - (int)Source.transform.localEulerAngles.z;
+		angle = angle % 360;
+		int flipCode = 0;
+		if (isVerticalMirror) {
+			if (requestIsFrontFacing) {
+
+				if (angle == 0) {
+					flipCode = 5;
+				} else if (angle == 90) {
+					flipCode = 6;
+				}
+				if (angle == 180) {
+					flipCode = 7;
+				} else if (angle == 270) {
+					flipCode = 8;
+				}
+			} else {
+				if (angle == 0) {
+					flipCode = -5;
+				} else if (angle == 90) {
+					flipCode = -6;
+				}
+				if (angle == 180) {
+					flipCode = -7;
+				} else if (angle == 270) {
+					flipCode = -8;
+				}
+			}
+		} else {
+			if (requestIsFrontFacing) {
+
+				if (angle == 0) {
+					flipCode = 1;
+				} else if (angle == 90) {
+					flipCode = 2;
+				}
+				if (angle == 180) {
+					flipCode = 3;
+				} else if (angle == 270) {
+					flipCode = 4;
+				}
+			} else {
+				if (angle == 0) {
+					flipCode = -1;
+				} else if (angle == 90) {
+					flipCode = -2;
+				}
+				if (angle == 180) {
+					flipCode = -3;
+				} else if (angle == 270) {
+					flipCode = -4;
+				}
+			}
+		}
+
+
+
+		Debug.Log ("FLIPCODE:  " + flipCode + "    ");
+
+		return flipCode;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+
+	void OnGUI ()
+	{
+		if (GUI.Button (new Rect (10, 10, 100, 50), "Click")) {
+			var rect = RectTransformToScreenSpace (Source.rectTransform, Camera.main);
+			Debug.Log (rect);
+
+
+			StartCoroutine (Capture (rect));
+			Target.material.SetInt ("_FlipStyle", ConvertCode ());
+		}
+	}
+
+
+	IEnumerator Capture (Rect rect)
+	{
+		 
+		yield return new WaitForEndOfFrame ();
+		yield return new WaitForEndOfFrame ();
+
+		//有些Android设备截图是黑色，需要设置QualitySettings.antiAliasing = 0;
+		Texture2D tex = new Texture2D ((int)rect.width, (int)rect.height, TextureFormat.RGB24, false);
+		tex.ReadPixels (rect, 0, 0, false);
+		tex.Apply ();
+
+		Target.texture = tex;
+		 
+
+	}
+
+
+	public static Rect RectTransformToScreenSpace (RectTransform transform, Camera camera)
+	{
+		var worldCorners = new Vector3[4];
+		transform.GetWorldCorners (worldCorners);
+
+		var screenPositions = new Vector3[4];
+
+		Vector2 min = new Vector2 (Screen.width, Screen.height);
+		Vector2 max = Vector2.zero;
+		for (int i = 0; i < worldCorners.Length; i++) {
+			var p = camera.WorldToScreenPoint (worldCorners [i]);
+
+			if (p.x < min.x) {
+				min.x = p.x;
+			}
+
+			if (p.y < min.y) {
+				min.y = p.y;
+			}
+
+			if (p.x > max.x) {
+				max.x = p.x;
+			}
+
+			if (p.y > max.y) {
+				max.y = p.y;
+			}
+
+		}
+
+
+		min.x = Mathf.Max (min.x, 0);
+		min.y = Mathf.Max (min.y, 0);
+		max.x = Mathf.Min (max.x, Screen.width);
+		max.y = Mathf.Min (max.y, Screen.height);
+
+		return new Rect (min.x, min.y, (max - min).x, (max - min).y);
+
+	}
+
+}
